eb105925e85c73029a43502448f539d5ada39216
Davy Peter Braun <hello@davybraun.com>
2018-02-11 11:20:00 +0100

    Add basic 'shooting' capabilities

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -8,6 +8,10 @@ public class RayShooter : MonoBehaviour {
 
   void Start() {
     _camera = GetComponent<Camera>();
+
+    // Hide mouse cursor at the center of the screen.
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
   }
 
   void Update() {
@@ -17,9 +21,25 @@ public class RayShooter : MonoBehaviour {
       Ray ray = _camera.ScreenPointToRay(point);
       RaycastHit hit;
       if (Physics.Raycast(ray, out hit)) {
-        Debug.Log("Hit " + hit.point);
+        StartCoroutine(SphereIndicator(hit.point));
       }
     }
+  }
+
+  void OnGUI() {
+    int size = 12;
+    float posX = _camera.pixelWidth / 2 - size / 4;
+    float posY = _camera.pixelHeight / 2 - size / 2;
+    GUI.Label(new Rect(posX, posY, size, size), "*");
+  }
+
+  private IEnumerator SphereIndicator(Vector3 pos) {
+    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    sphere.transform.position = pos;
+
+    yield return new WaitForSeconds(1);
+
+    Destroy(sphere);
   }
 
 }
