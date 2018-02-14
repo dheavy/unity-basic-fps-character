50422f590740304cd2d8c3191f171279b116fa64
Davy Peter Braun <hello@davybraun.com>
2018-02-14 16:15:56 +0100

    Enemy shoots fireball ate Player

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -8,6 +8,9 @@ public class WanderingAI : MonoBehaviour {
 
   private bool _alive;
 
+  [SerializeField] private GameObject fireballPrefab;
+  private GameObject _fireball;
+
   public void SetAlive(bool alive) {
     _alive = alive;
   }
@@ -24,8 +27,16 @@ public class WanderingAI : MonoBehaviour {
       // If any, turn towards a semirandom new direction.
       Ray ray = new Ray(transform.position, transform.forward);
       RaycastHit hit;
-      if (Physics.SphereCast(ray, 0.75f, out hit)) {
-        if (hit.distance < obstacleRange) {
+      if (Physics.SphereCast(ray, 1.75f, out hit)) {
+        GameObject hitObject = hit.transform.gameObject;
+        if (hitObject.GetComponent<PlayerCharacter>()) {
+          if (_fireball == null) {
+            _fireball = Instantiate(fireballPrefab) as GameObject;
+            // Place fireball in front of enemy and point in the same direction.
+            _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+            _fireball.transform.rotation = transform.rotation;
+          }
+        } else if (hit.distance < obstacleRange) {
           float angle = Random.Range(-110, 110);
           transform.Rotate(0, angle, 0);
         }
